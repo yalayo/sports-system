81c13018e60c71a80883e76968574dd7f063f502
yalayo <60822416+yalayo@users.noreply.github.com>
2024-08-16 08:52:32 +0000

    Initial implementation of the server base.

---
+++ bases/server/src/app/server/core.clj
@@ -0,0 +1,27 @@
+(ns app.server.core
+	(:require [com.stuartsierra.component :as component]
+						[io.pedestal.http :as http]
+						[environ.core :refer [env]]))
+
+(def routes #{["/" :get (fn [request] {:status 200 :body "Hello"}) :route-name :hello-world]})
+
+(defrecord Server []
+
+component/Lifecycle
+
+(start [this]
+			 (let [server (-> {::http/routes routes
+											 	 ::http/type :immutant
+												 ::http/host "0.0.0.0"
+											   ::http/join? false
+												 ::http/resource-path "public"
+											   ::http/port (Integer. (or (env :port) 5000))}
+											(http/default-interceptors)
+											(http/create-server)
+											(http/start))]
+			 (assoc this :server server)))
+
+(stop [this]
+			 (when-let [server (:server this)]
+				 (http/stop server))
+			 (assoc this :server nil)))
