70554d866b60f50f92bdd2d2c8c2514a6df9c78c
yalayo <60822416+yalayo@users.noreply.github.com>
2024-08-18 08:51:33 +0000

    Changes to merge routes comming from diferent components.

--- bases/server/src/app/server/core.clj
+++ bases/server/src/app/server/core.clj
@@ -1,17 +1,23 @@
 (ns app.server.core
 	(:require [com.stuartsierra.component :as component]
 						[io.pedestal.http :as http]
+						[io.pedestal.http.route :as route]
 						[environ.core :refer [env]]
-						[app.userui.interface :as user]))
+						[app.userui.interface :as user-ui]
+						[app.user.interface :as user-backend]))
 
-(def routes #{["/" :get (fn [request] {:status 200 :body "Hello"}) :route-name :hello-world]})
+(def routes
+	(route/expand-routes
+		(into #{}
+					(concat (user-ui/get-routes)
+									(user-backend/get-routes)))))
 
 (defrecord Server []
 
 component/Lifecycle
 
 (start [this]
-			 (let [server (-> {::http/routes (user/get-routes)
+			 (let [server (-> {::http/routes routes
 											 	 ::http/type :immutant
 												 ::http/host "0.0.0.0"
 											   ::http/join? false
